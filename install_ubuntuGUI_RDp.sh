b7145b3dc9b776eb97f6e34f08184925dfc10f60
AJ <138427234+Abhi-J@users.noreply.github.com>
2023-07-11 17:27:52 +0530

    Update install_ubuntuGUI_RDp.sh

--- install_ubuntuGUI_RDp.sh
+++ install_ubuntuGUI_RDp.sh
@@ -3,13 +3,7 @@
 # Install GUI packages
 sudo apt-get update
 sudo apt-get install ubuntu-desktop -y
-sudo apt install ubuntu-desktop 
-sudo apt install xrdp 
-sudo systemctl status xrdp 
-sudo adduser xrdp ssl-cert 
-sudo ufw allow 3389 (or) 
-sudo ufw allow from 192.168.2.0/24 to any port 3389 
-sudo ufw reload 
+
 # Configure VM to start GUI on boot
 sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 quiet splash"/' /etc/default/grub
 sudo update-grub
